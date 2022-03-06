188f0b8805e6bb550a3b6eb5104b252e6c90ee3a
Paul Van <paulyv8n@gmail.com>
2022-03-06 12:58:59 -0800

    added ability to insert version into sm url

--- tests.py
+++ tests.py
@@ -1,15 +1,19 @@
 #! /usr/bin/python3
 
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver')
+driver = webdriver.Chrome(executable_path='./drivers/chromedriver')
 
 def expand_shadow_element(element):
   shadow_root = driver.execute_script('return arguments[0].shadowRoot', element)
   return shadow_root
 
-def test():
-  driver.get('https://devicecertification.youtube/?nonprodApi=true')
+def login(version=None):
+  url = 'https://versiondevicecertification.youtube/?nonprodApi=true'
+  ver = "" if version == None else sys.argv[1]+'.'
+  url = url.replace('version', ver)
+  driver.get(url)
   root1 = driver.find_element(By.TAG_NAME, 'saltmine-app')
   shadow_root1 = expand_shadow_element(root1)
   root2 = shadow_root1.find_element(By.TAG_NAME, 'login-page')
@@ -17,8 +21,7 @@ def test():
   btn = shadow_root2.find_element(By.TAG_NAME, 'paper-button')
   btn.click()
 
-#  shadow_content = shadow_root.find_element(By.TAG_NAME, 'paper-button') #shadow_content needs to be actual element trying to target
-
-
-
-test()
+if len(sys.argv) == 1:
+  login()
+else:
+  login(sys.argv[1])
