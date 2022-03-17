e6a3d645a3ca83e209aaa148d53c52d163c2301d
pauly <paulyv8n@gmail.com>
2022-03-17 07:10:30 +0000

    first test discover

--- CLI/runner.py
+++ CLI/runner.py
@@ -1,14 +1,12 @@
 #! /usr/bin/python3
 
 import pytest
+from pathlib import Path
 import tests
 
 
-def test_discover():
-  assert tests.ytsDiscover() == 'Searching for devices', "Discover Utility searches"
+def assert_discover():
+  path = Path('/home/pauly/.local/share/yts_server/devices.json')
+  assert tests.ytsDiscover() == 'Searching for devices' and path.is_file()
 
-
-def test_num():
-  x=2
-  assert x==2
 
