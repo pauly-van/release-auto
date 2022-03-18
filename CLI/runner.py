17a454bb7f2fa1dcce777c6622281961d255e9c3
pauly <paulyv8n@gmail.com>
2022-03-18 10:22:26 +0000

    added launch, stop, and test tests

--- CLI/runner.py
+++ CLI/runner.py
@@ -1,12 +1,19 @@
 #! /usr/bin/python3
 
-import pytest
+import pytest, subprocess
 from pathlib import Path
 import tests
 
+def test_cliVersion():
+  versions = tests.ytsVersion()
+  assert versions[0] == versions[1]
 
-def assert_discover():
+def test_cliDiscover():
   path = Path('/home/pauly/.local/share/yts_server/devices.json')
   assert tests.ytsDiscover() == 'Searching for devices' and path.is_file()
 
+def test_cliLaunch():
+  assert tests.ytsLaunch() == 'Launch request sent'
 
+def test_cliStop():
+  assert tests.ytsStop() == 'Stop request sent'
--- CLI/tests.py
+++ CLI/tests.py
@@ -1,8 +1,39 @@
 #! /usr/bin/python3
 
-import subprocess
+import subprocess, os, json
+
+subprocess.call("yts discover", shell=True)
+
+def id():
+  with open('/home/pauly/.local/share/yts_server/devices.json', 'r') as file:
+    data = json.load(file)
+    if data == []:
+      raise ValueError('No devices were found to test CLI commands')
+    return data[0]['shortId']
+  
+def ytsVersion():
+  os.chdir('/home/pauly') 
+  os.system("curl -O -L https://dev.yts.devicecertification.youtube/yts_server.zip; rm -rf yts_server; unzip yts_server.zip -d yts_server")
+  cliVer = subprocess.check_output("yts --version", shell=True)
+  actual = subprocess.check_output("curl http://dev.yts.devicecertification.youtube/version", shell=True)
+  return cliVer.decode('UTF-8')[4:len(cliVer)-1], actual.decode('UTF-8')[0:]
 
 def ytsDiscover():
   devices = subprocess.check_output("yts discover", shell=True)
   return devices.decode('UTF-8')[:21]
+
+def ytsLaunch():
+  output = subprocess.check_output("yts launch %s 'https://www.youtube.com/tv?v=9szn1QQfas'" % (id()), shell=True)  
+  return output.decode('UTF-8')[0:19]
+
+def ytsStop():
+  ID = id()
+  subprocess.call("yts launch %s ''" % (ID), shell=True)
+  output = subprocess.check_output("yts stop %s" % (ID), shell=True)
+  return output.decode('UTF-8')[0:17]
+
+def ytsTest():
+  ID = id()
+  output = subprocess.check_output("yts test %s 'DOM CSS Tests'" % (ID), shell=True)
+  print(output.decode('UTF-8'))
 
