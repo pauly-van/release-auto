5680eb2773d92517e4617891590de890599bb845
Paul Van <paulyv8n@gmail.com>
2022-06-05 18:30:40 -0700

    autopep8 styling

--- CLI/runner.py
+++ CLI/runner.py
@@ -1,45 +1,70 @@
 #! /usr/bin/python3
 
-import unittest, subprocess
+import unittest
+import subprocess
 from pathlib import Path
 import tests
 
+
 class TestCliCommands(unittest.TestCase):
-      def test_version_cmd(self):
+    def test_version_cmd(self):
         versions = tests.yts_version()
         self.assertEqual(versions[0], versions[1])
 
-      def test_discover_cmd(self):
-        path = Path('/Users/paulnguyen/Library/Preferences/yts_server/devices.json')
-        self.assertEqual(tests.yts_discover(),'Searching for devices', "Discover command failed to search for devices") and path.is_file()
+    def test_discover_cmd(self):
+        path = Path(
+            '/Users/paulnguyen/Library/Preferences/yts_server/devices.json')
+        self.assertEqual(
+            tests.yts_discover(),
+            'Searching for devices',
+            "Discover command failed to search for devices") and path.is_file()
 
-      def test_launch_cmd(self):
-        self.assertEqual(tests.yts_launch(), 'Launch request sent', 'Launch command did not execute successfully')
+    def test_launch_cmd(self):
+        self.assertEqual(
+            tests.yts_launch(),
+            'Launch request sent',
+            "Launch command did not execute successfully")
 
-      def test_stop_cmd(self):
-        self.assertEqual(tests.yts_stop(), 'Stop request sent', "Stop command did not execute successfully")
+    def test_stop_cmd(self):
+        self.assertEqual(
+            tests.yts_stop(),
+            'Stop request sent',
+            "Stop command did not execute successfully")
 
-      def test_test_cmd(self):
-        self.assertEqual(tests.yts_test(), "Executed 1 of 1 test", "Test command did not execute")
+    def test_test_cmd(self):
+        self.assertEqual(
+            tests.yts_test(),
+            "Executed 1 of 1 test",
+            "Test command did not execute")
 
-      def test_list_cmd(self):
-        self.assertEqual(tests.yts_list(), "Listed 1219 test(s).", "List command did not execute or wrong number of tests")
+    def test_list_cmd(self):
+        self.assertEqual(
+            tests.yts_list(),
+            "Listed 1219 test(s).",
+            "List command did not execute or wrong number of tests")
 
-      def test_cert_cmd(self):
-        self.assertEqual(tests.yts_cert(), "Executed 1 of 1 test SUCCESS.\n", "Cert command did not execute")
+    def test_cert_cmd(self):
+        self.assertEqual(
+            tests.yts_cert(),
+            "Executed 1 of 1 test SUCCESS.\n",
+            "Cert command did not execute")
 
-      def test_login_user_cmd(self):
+    def test_login_user_cmd(self):
         pass
 
-      def test_credits_cmd(self):
+    def test_credits_cmd(self):
         pass
 
-      def test_update_cmd(self):
+    def test_update_cmd(self):
         versions = tests.yts_update()
-        self.assertNotEqual(versions[0], versions[1], "Both versions are the same after update command")
+        self.assertNotEqual(
+            versions[0],
+            versions[1],
+            "Both versions are the same after update command")
 
-      def test_evergreen_channel_cmd(self):
+    def test_evergreen_channel_cmd(self):
         pass
-      
+
+
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
--- CLI/tests.py
+++ CLI/tests.py
@@ -1,65 +1,88 @@
 #! /usr/bin/python3
 
-import subprocess, os, json
+import subprocess
+import os
+import json
 
+#comment out for testing, uncomment when everything functioning
 #subprocess.call("yts discover", shell=True)
 
+
 def id():
-  with open('/Users/paulnguyen/Library/Preferences/yts_server/devices.json', 'r') as file:
-    data = json.load(file)
-    if data == []:
-      raise ValueError('No devices were found to test CLI commands')
-    return data[0]['shortId']
+    with open('/Users/paulnguyen/Library/Preferences/yts_server/devices.json', 'r') as file:
+        data = json.load(file)
+        if data == []:
+            raise ValueError('No devices were found to test CLI commands')
+        return data[0]['shortId']
+
 
 ID = id()
-  
+
+
 def yts_version():
-  os.chdir('/Users/paulnguyen') 
-  os.system("curl -O -L https://dev.yts.devicecertification.youtube/yts_server.zip; rm -rf yts_server; unzip yts_server.zip -d yts_server")
-  cliVer = subprocess.check_output("yts --version", shell=True)
-  actual = subprocess.check_output("curl http://dev.yts.devicecertification.youtube/version", shell=True)
-  return cliVer.decode('UTF-8')[4:len(cliVer)-1], actual.decode('UTF-8')[0:]
+    os.chdir('/Users/paulnguyen')
+    os.system("curl -O -L https://dev.yts.devicecertification.youtube/yts_server.zip; rm -rf yts_server; unzip yts_server.zip -d yts_server")
+    cliVer = subprocess.check_output("yts --version", shell=True)
+    actual = subprocess.check_output(
+        "curl http://dev.yts.devicecertification.youtube/version", shell=True)
+    return cliVer.decode('UTF-8')[4:len(cliVer) -1], actual.decode('UTF-8')[0:]
+
 
 def yts_discover():
-  devices = subprocess.check_output("yts discover", shell=True)
-  return devices.decode('UTF-8')[:21]
+    devices = subprocess.check_output("yts discover", shell=True)
+    return devices.decode('UTF-8')[:21]
+
 
 def yts_launch():
-  output = subprocess.check_output(f"yts launch {ID} 'https://www.youtube.com/tv?v=9szn1QQfas'", shell=True)  
-  return output.decode('UTF-8')[0:19]
+    output = subprocess.check_output(
+        f"yts launch {ID} 'https://www.youtube.com/tv?v=9szn1QQfas'", shell=True)
+    return output.decode('UTF-8')[0:19]
+
 
 def yts_stop():
-  subprocess.call(f"yts launch {ID} ''", shell=True)
-  output = subprocess.check_output(f"yts stop {ID}", shell=True)
-  return output.decode('UTF-8')[0:17]
+    subprocess.call(f"yts launch {ID} ''", shell=True)
+    output = subprocess.check_output(f"yts stop {ID}", shell=True)
+    return output.decode('UTF-8')[0:17]
+
 
 def yts_test():
-  output = subprocess.check_output(f"yts test {ID} 'DOM CSS Tests CSS Media Rule CSSMediaRule.cssRules'", shell=True)
-  return output.decode('UTF-8')[len(output)-51:len(output)-31]
+    output = subprocess.check_output(
+        f"yts test {ID} 'DOM CSS Tests CSS Media Rule CSSMediaRule.cssRules'",
+        shell=True)
+    return output.decode('UTF-8')[len(output) - 51:len(output) - 31]
+
 
 def yts_list():
-  output = subprocess.check_output("yts list", shell=True)
-  return output.decode("UTF-8")[len(output)-21:len(output)-1]
+    output = subprocess.check_output("yts list", shell=True)
+    return output.decode("UTF-8")[len(output) - 21:len(output) - 1]
+
 
 def yts_cert():
-  output = subprocess.check_output(f"yts cert {ID} 'DOM CSS Tests CSS Rule List CSSRuleList.item' --rerun", shell=True)
-  return output.decode("UTF-8")[len(output)-45:len(output)-19]
+    output = subprocess.check_output(
+        f"yts cert {ID} 'DOM CSS Tests CSS Rule List CSSRuleList.item' --rerun",
+        shell=True)
+    return output.decode("UTF-8")[len(output) - 45:len(output) - 19]
+
 
 def yts_login():
-  pass
+    pass
+
 
 def yts_user():
-  pass
+    pass
+
 
 def yts_credits():
-  pass
+    pass
+
 
 def yts_update():
-  current_version = subprocess.check_output('yts --version', shell=True)
-  subprocess.call('yts update', shell=True)
-  # this will revert back to prod version
-  updated_version = subprocess.check_output('yts --version', shell=True)
-  return (current_version, updated_version)
+    current_version = subprocess.check_output('yts --version', shell=True)
+    subprocess.call('yts update', shell=True)
+    # this will revert back to prod version
+    updated_version = subprocess.check_output('yts --version', shell=True)
+    return (current_version, updated_version)
+
 
 def yts_evergreen_channel_and_update():
-  pass
+    pass
