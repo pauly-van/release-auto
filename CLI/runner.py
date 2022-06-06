8db12217af2d7476f517d5f3a74bf08ffbda4b03
Paul Van <paulyv8n@gmail.com>
2022-06-05 17:49:04 -0700

    added error messages for test cases

--- CLI/runner.py
+++ CLI/runner.py
@@ -11,22 +11,22 @@ class TestCliCommands(unittest.TestCase):
 
       def test_discover_cmd(self):
         path = Path('/Users/paulnguyen/Library/Preferences/yts_server/devices.json')
-        self.assertEqual(tests.yts_discover(),'Searching for devices') and path.is_file()
+        self.assertEqual(tests.yts_discover(),'Searching for devices', "Discover command failed to search for devices") and path.is_file()
 
       def test_launch_cmd(self):
-        self.assertEqual(tests.yts_launch(), 'Launch request sent', 'Launch successful')
+        self.assertEqual(tests.yts_launch(), 'Launch request sent', 'Launch command did not execute successfully')
 
       def test_stop_cmd(self):
-        self.assertEqual(tests.yts_stop(), 'Stop request sent')
+        self.assertEqual(tests.yts_stop(), 'Stop request sent', "Stop command did not execute successfully")
 
       def test_test_cmd(self):
-        self.assertEqual(tests.yts_test(), "Executed 1 of 1 test")
+        self.assertEqual(tests.yts_test(), "Executed 1 of 1 test", "Test command did not execute")
 
       def test_list_cmd(self):
-        self.assertEqual(tests.yts_list(), "Listed 1219 test(s).")
+        self.assertEqual(tests.yts_list(), "Listed 1219 test(s).", "List command did not execute or wrong number of tests")
 
       def test_cert_cmd(self):
-        self.assertEqual(tests.yts_cert(), "Executed 1 of 1 test SUCCESS.\n")
+        self.assertEqual(tests.yts_cert(), "Executed 1 of 1 test SUCCESS.\n", "Cert command did not execute")
 
       def test_login_user_cmd(self):
         pass
@@ -36,7 +36,7 @@ class TestCliCommands(unittest.TestCase):
 
       def test_update_cmd(self):
         versions = tests.yts_update()
-        self.assertNotEqual(versions[0], versions[1])
+        self.assertNotEqual(versions[0], versions[1], "Both versions are the same after update command")
 
       def test_evergreen_channel_cmd(self):
         pass
